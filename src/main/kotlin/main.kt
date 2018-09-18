93e8965ab6a41c1d52a4127bb34cad38d18ac512
Magnus Kühn <magnus.khn@gmail.com>
2018-09-18 09:52:02 +0200

    Eliminate coroutines

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,3 @@
-import kotlinx.coroutines.experimental.GlobalScope
-import kotlinx.coroutines.experimental.asCoroutineDispatcher
-import kotlinx.coroutines.experimental.launch
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.TelegramBotsApi
@@ -19,8 +16,8 @@ fun main(args: Array<String>) {
     api.registerBot(LaTeXBot())
 }
 
-private val tempDirThreadDispatcher = Executors.newFixedThreadPool(
-        Runtime.getRuntime().availableProcessors() - 1, ::TempDirThread).asCoroutineDispatcher()
+private val threadPool = Executors.newFixedThreadPool(
+        Runtime.getRuntime().availableProcessors() - 1, ::TempDirThread)
 
 class TempDirThread(target: Runnable) : Thread(target) {
     val tmpDir = createTempDir()
@@ -29,8 +26,43 @@ class TempDirThread(target: Runnable) : Thread(target) {
 }
 
 
+private val template = LaTeXBot::class.java.getResource("/template.tex").readText()
+
+data class ProcessingJob(val bot: TelegramLongPollingBot, val message: Message, val code: String) : Runnable {
+    override fun run() {
+        val thread = Thread.currentThread() as TempDirThread
+        thread.latexFile.writer().use {
+            it.write(template)
+            it.write(code)
+            it.write("\n\\end{document}")
+        }
+
+        try {
+            val compilation = ProcessBuilder("pdflatex", "-interaction=nonstopmode", thread.latexFile.name)
+                    .directory(thread.tmpDir)
+                    .inheritIO().start()
+            bot.execute(SendChatAction(message.chatId, "upload_photo"))
+            compilation.waitFor(20, TimeUnit.SECONDS)
+
+            if (compilation.exitValue() == 0) {
+                val convert = ProcessBuilder("convert", "-density", "300", "main.pdf", "-quality", "90", "main.png")
+                        .directory(thread.tmpDir)
+                        .inheritIO().start()
+                bot.execute(SendChatAction(message.chatId, "upload_photo"))
+
+                convert.waitFor(10, TimeUnit.SECONDS)
+                bot.execute(SendPhoto()
+                        .setChatId(message.chatId)
+                        .setPhoto(thread.outputFile)
+                        .setReplyToMessageId(message.messageId))
+            }
+        } catch (e: InterruptedException) {
+        }
+    }
+}
+
+
 class LaTeXBot : TelegramLongPollingBot() {
-    private val template = LaTeXBot::class.java.getResource("/template.tex").readText()
     private val userWhitelist = System.getenv("USER_WHITELIST").split(",").map { it.toInt() }.toSet()
 
     override fun onUpdateReceived(update: Update) {
@@ -38,49 +70,16 @@ class LaTeXBot : TelegramLongPollingBot() {
         if (update.hasMessage() && update.message.text != null && !update.message.text.isEmpty() &&
                 userWhitelist.contains(update.message.from.id)) {
             if (update.message.text.startsWith("/tex@RenderLaTeXBot ")) {
-                process(update.message, update.message.text.drop(20))
+                threadPool.submit(ProcessingJob(this, update.message, update.message.text.drop(20)))
             } else if (update.message.text.startsWith("/tex ")) {
-                process(update.message, update.message.text.drop(5))
+                threadPool.submit(ProcessingJob(this, update.message, update.message.text.drop(5)))
             } else if (update.message.text.startsWith("@RenderLaTeXBot ")) {
-                process(update.message, update.message.text.drop(16))
+                threadPool.submit(ProcessingJob(this, update.message, update.message.text.drop(16)))
             } else if (update.message.isUserMessage || update.message.text.contains("\\")
                     || update.message.text.contains("$")) {
-                process(update.message, update.message.text)
+                threadPool.submit(ProcessingJob(this, update.message, update.message.text))
             }
         }
-    }
-
-    private fun process(message: Message, code: String) {
-        GlobalScope.launch(tempDirThreadDispatcher) {
-            val thread = Thread.currentThread() as TempDirThread
-            thread.latexFile.writer().use {
-                it.write(template)
-                it.write(code)
-                it.write("\n\\end{document}")
-            }
-
-            try {
-                val compilation = ProcessBuilder("pdflatex", "-interaction=nonstopmode", thread.latexFile.name)
-                        .directory(thread.tmpDir)
-                        .inheritIO().start()
-                compilation.waitFor(20, TimeUnit.SECONDS)
-
-                if (compilation.exitValue() == 0) {
-                    ProcessBuilder("convert", "-density", "300", "main.pdf", "-quality", "90", "main.png")
-                            .directory(thread.tmpDir)
-                            .inheritIO().start()
-                            .waitFor(10, TimeUnit.SECONDS)
-
-                    execute(SendPhoto()
-                            .setChatId(message.chatId)
-                            .setPhoto(thread.outputFile)
-                            .setReplyToMessageId(message.messageId))
-                }
-            } catch (e: InterruptedException) {
-            }
-        }
-
-        execute(SendChatAction(message.chatId, "upload_photo"))
     }
 
     override fun getBotToken() = System.getenv("BOT_TOKEN")
